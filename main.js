ece6dc51c70048208fd6204b76c99a9f72006f55
Florian Valence <fantlo@gmail.com>
2013-05-20 21:05:50 +0200

    Data image type Fix
    The first part of the data of the image was missng, fixed

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ define(function (require, exports, module) {
 
 	// Brackets modules
 	var EditorManager       = brackets.getModule("editor/EditorManager"),
-		ProjectManager      = brackets.getModule("project/ProjectManager"),
 		DocumentManager     = brackets.getModule("document/DocumentManager"),
 		NativeFileSystem    = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
 		CommandManager      = brackets.getModule("command/CommandManager"),
@@ -41,18 +40,6 @@ define(function (require, exports, module) {
 	// local modules
 	var mainDialog       = require("text!dialog.html");
 
-	function selectAll($id) {
-		/*document.getElementById(id).focus();
-		document.getElementById(id).select();*/
-		$id.focus().select();
-		console.log($id);
-	}
-	$("textarea").focus(function () {
-		// Check for the change
-		if (this.value == this.defaultValue) {
-			this.select();
-		}
-	});
 	function getBase64Image(URL) {
 		var imgee = new Image();
 		imgee.src = URL;
@@ -69,12 +56,7 @@ define(function (require, exports, module) {
 
 			var dataURL = canvas.toDataURL("image/png");
 
-			console.log(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-			var dataaa = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-			$(".modal-body").append("<textarea id='data' style='height:80px; width:100%'>" + dataaa + "</textarea>");
-			selectAll($('#data'));
-			$("#data").on('click', selectAll($('#data')));
-
+			$(".modal-body").append("<textarea id='data' style='height:80px; width:100%'>" + dataURL + "</textarea>");
 		};
 	}
 //    test url    http://pages.github.com/images/scrcap-author.png
@@ -95,18 +77,16 @@ define(function (require, exports, module) {
 		$dlg = $(mainDialog);
 		Dialogs.showModalDialogUsingTemplate($dlg);
 		// we implement our own OK button handler so we have
-		// no interest in the returned promise
 
 		// URL input
 		$getUrlControl = $dlg.find(".get-url");
 
 		// add OK button handler
-		$dlg.one("click", ".dialog-button-ext", function (e) {
+		$dlg.on("click", ".dialog-button-ext", function (e) {
 			var imgUrl = $getUrlControl.val();
 			console.log("Sucking " + imgUrl);
 			loadImage(imgUrl);
 		});
-
 	}
 
 	CommandManager.register("Edit File", COMMAND_ID, launchUrlDialog);
