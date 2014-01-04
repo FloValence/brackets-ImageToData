e9c56e8a293fb3ee6341db2a3675721cd2750cee
FloValence <florian.m.valence@gmail.com>
2014-01-04 16:30:28 +0100

    on prog - detec image open

--- main.js
+++ main.js
@@ -22,7 +22,7 @@
  */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, brackets, $, Image, document, require, Mustache, window */
+/*global define, brackets, $, Image, document, require, Mustache, window, PathUtils */
 
 require.config({
     paths: {
@@ -33,71 +33,72 @@ require.config({
 });
 
 define(function (require, exports, module) {
-	"use strict";
-	var COMMAND_ID = "getimage.convertit";
-
-	// Brackets modules
-	var AppInit				= brackets.getModule("utils/AppInit"),
-		EditorManager       = brackets.getModule("editor/EditorManager"),
-		ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
-		DocumentManager     = brackets.getModule("document/DocumentManager"),
-		CommandManager      = brackets.getModule("command/CommandManager"),
-		KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
-		Menus				= brackets.getModule("command/Menus"),
-		Dialogs             = brackets.getModule("widgets/Dialogs"),
-		Strings             = brackets.getModule("strings");
-
-	// local modules
-	var mainDialog		= require("text!html/itd-dialog.html"),
-		idtToolbarHtml	= require("text!html/itd-toolbar.html");
-
-
-	var Trad             = require("strings");
-
-	var $toolbarIcon	= $(idtToolbarHtml),
-		$mainDialog;
-
-	function loadImage(imgUrl, $getBodyControl, $getload) {
-		// The convert to data trick
-		var imgee = new Image();
-		imgee.src = imgUrl;
-		imgee.onload = function () {
-
-			var canvas = document.createElement("canvas");
-			canvas.width = this.width;
-			canvas.height = this.height;
-
-			var ctx = canvas.getContext("2d");
-			ctx.drawImage(this, 0, 0);
-
-			var dataURL = canvas.toDataURL("image/png");
-			// Finally showing the result
-			$getBodyControl.append(Trad.COPY_THAT + " : <br><textarea class='data'>" + dataURL + "</textarea><br>" + Trad.IMAGE_CONV + " : <br><img class='sucked' src='" + imgUrl + "'>");
-		};
-	}
+    "use strict";
+    var COMMAND_ID = "getimage.convertit";
+    
+    // Brackets modules
+    var AppInit				= brackets.getModule("utils/AppInit"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        CommandManager      = brackets.getModule("command/CommandManager"),
+        KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
+        Menus				= brackets.getModule("command/Menus"),
+        Dialogs             = brackets.getModule("widgets/Dialogs"),
+        Strings             = brackets.getModule("strings");
+    
+    // local modules
+    var mainDialog		= require("text!html/itd-dialog.html"),
+        idtToolbarHtml	= require("text!html/itd-toolbar.html");
+    
+    var Trad             = require("strings");
+    
+    var $toolbarIcon	= $(idtToolbarHtml),
+        $mainDialog;
+    
+    var currentDoc;
+    
+    function loadImage(imgUrl, $getBodyControl, $getload) {
+        // The convert to data trick
+        var imgee = new Image();
+        imgee.src = imgUrl;
+        imgee.onload = function () {
+            
+            var canvas = document.createElement("canvas");
+            canvas.width = this.width;
+            canvas.height = this.height;
+            
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(this, 0, 0);
+            
+            var dataURL = canvas.toDataURL("image/png");
+            // Finally showing the result
+            $getBodyControl.append(Trad.COPY_THAT + " : <br><textarea class='data'>" + dataURL + "</textarea><br>" + Trad.IMAGE_CONV + " : <br><img class='sucked' src='" + imgUrl + "'>");
+        };
+    }
     
     function localPath(img) {
         //return img;
         
         return window.URL.createObjectURL(img);
     }
-
-	function launchUrlDialog() {
-
-		var $dlg,
-			$getUrlControl,
+    
+    function launchUrlDialog() {
+        
+        var $dlg,
+            $getUrlControl,
             $getLocalUrlControl,
             $getLocalLabel,
-			$getBodyControl,
-			$getload;
-
-		$dlg = $mainDialog;
-
-		Dialogs.showModalDialogUsingTemplate($dlg);
-
-		// URL input
-		$getUrlControl = $dlg.find(".get-url");
-		$getUrlControl.focus();
+            $getBodyControl,
+            $getload;
+        
+        $dlg = $mainDialog;
+        
+        Dialogs.showModalDialogUsingTemplate($dlg);
+        
+        // URL input
+        $getUrlControl = $dlg.find(".get-url");
+        $getUrlControl.focus();
         
         // File input
         $getLocalUrlControl = $dlg.find(".get-local");
@@ -107,38 +108,69 @@ define(function (require, exports, module) {
             $getLocalLabel.html(image.name);
             $getUrlControl.html("");
         });
-
-		// ModalBody
-		$getBodyControl = $dlg.find(".data-show");
-
-		// Loading Image
-		$getload = $dlg.find(".loading");
-
-		// add OK button handler
-		$dlg.on("click", ".dialog-button-ext", function () {
-			$getBodyControl.empty();
-			var imgUrl = $getUrlControl.val() || localPath($getLocalUrlControl[0].files[0]) || "";
-			// Ready ? Let's go !
-			if (imgUrl !== "") {
-				loadImage(imgUrl, $getBodyControl, $getload);
-			}
-		});
-	}
-
-	// load everything when brackets is done loading
-	AppInit.appReady(function () {
-		$toolbarIcon.appendTo("#main-toolbar .buttons");
-		$toolbarIcon.on('click', launchUrlDialog);
-		// CSS
-		ExtensionUtils.loadStyleSheet(module, "style/style.css");
-	});
-
-	CommandManager.register(Trad.COMMAND_NAME, COMMAND_ID, launchUrlDialog);
-	KeyBindingManager.addBinding(COMMAND_ID, "Alt-Shift-G");
-
-	var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
-	menu.addMenuItem(COMMAND_ID);
-
-	$mainDialog = $(Mustache.render(mainDialog, Trad));
-
+        
+        // ModalBody
+        $getBodyControl = $dlg.find(".data-show");
+        
+        // Loading Image
+        $getload = $dlg.find(".loading");
+        
+        // add OK button handler
+        $dlg.on("click", ".dialog-button-ext", function () {
+            $getBodyControl.empty();
+            var imgUrl = $getUrlControl.val() || localPath($getLocalUrlControl[0].files[0]) || "";
+            // Ready ? Let's go !
+            if (imgUrl !== "") {
+                loadImage(imgUrl, $getBodyControl, $getload);
+            }
+        });
+    }
+    
+    function _setPanelVisibility(isVisible) {
+        console.log(isVisible);
+    }
+    
+    function currentDocChangedHandler() {
+        var doc = DocumentManager.getCurrentDocument(),
+            ext = doc ? PathUtils.filenameExtension(doc.file.fullPath).toLowerCase() : "";
+        
+        console.log(DocumentManager.getCurrentDocument());
+        
+        if (currentDoc) {
+            //$(currentDoc).off("change", _documentChange);
+            currentDoc = null;
+        }
+        
+        if (doc && /jpg|png|gif/.test(ext)) {
+            currentDoc = doc;
+            console.log(doc);
+            //$(currentDoc).on("change", _documentChange);
+            //$icon.css({display: "block"});
+            _setPanelVisibility("visible");
+            //_loadDoc(doc);
+        } else {
+            //$icon.css({display: "none"});
+            _setPanelVisibility(false);
+        }
+    }
+    
+    // Add a document change handler
+    $(DocumentManager).on("currentDocumentChange", currentDocChangedHandler);
+    
+    // load everything when brackets is done loading
+    AppInit.appReady(function () {
+        $toolbarIcon.appendTo("#main-toolbar .buttons");
+        $toolbarIcon.on('click', launchUrlDialog);
+        // CSS
+        ExtensionUtils.loadStyleSheet(module, "style/style.css");
+    });
+    
+    CommandManager.register(Trad.COMMAND_NAME, COMMAND_ID, launchUrlDialog);
+    KeyBindingManager.addBinding(COMMAND_ID, "Alt-Shift-G");
+    
+    var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
+    menu.addMenuItem(COMMAND_ID);
+    
+    $mainDialog = $(Mustache.render(mainDialog, Trad));
+    
 });
